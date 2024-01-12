759514056521dcae3a680d177f128292c53528c4
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2024-01-12 14:38:47 +0100

    corrected focus

--- darkmode.js
+++ darkmode.js
@@ -2,13 +2,15 @@ const lightModeButton = document.getElementById("light-mode");
 const darkModeButton = document.getElementById("dark-mode");
 
 const turnToDarkMode = () => {
-  darkModeButton.classList.add("hidden");
   lightModeButton.classList.remove("hidden");
+  lightModeButton.focus();
+  darkModeButton.classList.add("hidden");
   document.body.classList.add("dark-mode");
 };
 
 const turnToLightMode = () => {
   darkModeButton.classList.remove("hidden");
+  darkModeButton.focus();
   lightModeButton.classList.add("hidden");
   document.body.classList.remove("dark-mode");
 };
